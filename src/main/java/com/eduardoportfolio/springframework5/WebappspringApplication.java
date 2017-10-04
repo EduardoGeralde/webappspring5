3960636b41a3dbdc9928f472fc2d24fdd9e83db8
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-10-04 10:16:48 -0300

    first commit

---
+++ src/main/java/com/eduardoportfolio/springframework5/WebappspringApplication.java
@@ -0,0 +1,12 @@
+package com.eduardoportfolio.springframework5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappspringApplication.class, args);
+	}
+}
