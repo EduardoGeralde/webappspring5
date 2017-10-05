2cb67dc17d589fe86ab918b324ff185e98cd26b9
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-10-05 14:52:07 -0300

    Publisher class created

--- src/main/java/com/eduardoportfolio/springframework5/model/Publisher.java
+++ src/main/java/com/eduardoportfolio/springframework5/model/Publisher.java
@@ -1,7 +1,65 @@
 package com.eduardoportfolio.springframework5.model;
 
+import javax.persistence.*;
+
 /**
  * Created by Eduardo on 05/10/17.
  */
+
+@Entity
 public class Publisher {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    Long id;
+    String name;
+    String address;
+
+    public Publisher() {
+    }
+
+    public Publisher(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Publisher publisher = (Publisher) o;
+
+        return id != null ? id.equals(publisher.id) : publisher.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
