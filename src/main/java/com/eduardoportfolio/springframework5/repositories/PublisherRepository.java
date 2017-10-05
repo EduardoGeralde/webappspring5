ba913093ce9392d494a7f6c5ddda06969c650c63
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-10-05 14:53:06 -0300

    publisher repository interface created

--- src/main/java/com/eduardoportfolio/springframework5/repositories/PublisherRepository.java
+++ src/main/java/com/eduardoportfolio/springframework5/repositories/PublisherRepository.java
@@ -1,7 +1,10 @@
 package com.eduardoportfolio.springframework5.repositories;
 
+import com.eduardoportfolio.springframework5.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
 /**
  * Created by Eduardo on 05/10/17.
  */
-public interface PublisherRepository {
+public interface PublisherRepository extends CrudRepository<Publisher,Long>{
 }
